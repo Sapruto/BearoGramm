8fb82bb3f282401e5a8aac7c78161eab8af1fb7f
Sapruto <alex.sapryka@gmail.com>
2026-09-08 20:50:00 +0300

    I deleted folder types in messages, becuase I VINES this in the general becuas this is shared module

--- backend/src/modules/chats/chat_types/personal/personal_models.py
+++ backend/src/modules/chats/chat_types/personal/personal_models.py
@@ -1,5 +1,5 @@
-from typing import Optional, List, Dict, Any
-from pydantic import BaseModel, Field
+from typing import List
+from pydantic import BaseModel
 from datetime import datetime
 
 from ..chat_types import ChatType
