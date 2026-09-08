a55bbb457c9dda45ff32b4db08357fa2be9b463b
Sapruto <alex.sapryka@gmail.com>
2026-09-08 21:30:56 +0300

    You are black stuped monkie(dima)

--- backend/src/general/processors/text/text_data.py
+++ backend/src/general/processors/text/text_data.py
@@ -1,9 +1,9 @@
 from pydantic import Field
-from src.modules.messages.types.base.base_message_data import BaseMessageData
+from ..base.base_data import BaseData
 
 TextTypeName = "text_message_type"
 
 
-class TextData(BaseMessageData):
+class TextData(BaseData):
     data_type: str = Field(default=TextTypeName)
     text: str = Field(default="")
