cddfad68d1943f119f478f47d2a1932e7ab29fc3
Sapruto <alex.sapryka@gmail.com>
2026-09-09 23:27:29 +0300

    I don't know why this

--- backend/src/core/paths.py
+++ backend/src/core/paths.py
@@ -3,8 +3,7 @@ from functools import lru_cache
 
 PROJECT_ROOT = Path(__file__).parent.parent.parent
 LOGS_ROOT = PROJECT_ROOT / "logs"
-TEMPLATES_ROOT = PROJECT_ROOT / "frontend" / "templates"
-STATIC_ROOT = PROJECT_ROOT / "frontend" / "static"
+STATIC_ROOT = PROJECT_ROOT / "static"
 ENV_PATH = PROJECT_ROOT / ".env"
 CONFIG_ROOT = PROJECT_ROOT / "configs"
 
@@ -15,16 +14,11 @@ def in_logs(subpath: str = "") -> Path:
     return LOGS_ROOT / subpath
 
 
-def in_temp(subpath: str = "") -> Path:
-    return TEMPLATES_ROOT / subpath
-
-
 @lru_cache(maxsize=128)
 def get_project_file(*parts) -> Path:
     return PROJECT_ROOT.joinpath(*parts)
 
 
 LOGS_ROOT.mkdir(parents=True, exist_ok=True)
-TEMPLATES_ROOT.mkdir(parents=True, exist_ok=True)
 STATIC_ROOT.mkdir(parents=True, exist_ok=True)
 DATABASE_ROOT.mkdir(parents=True, exist_ok=True)
