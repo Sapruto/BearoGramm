9acbd5894b05925989b6ae6290fd736a21335f4a
Саня <alex.sapryka@gmail.com>
2026-09-11 13:45:02 +0300

    Update default_avatar_randomizer.py

--- backend/src/modules/profiles_custom/models/orm/default_avatar_randomizer.py
+++ backend/src/modules/profiles_custom/models/orm/default_avatar_randomizer.py
@@ -1,10 +1,25 @@
 from random import choice
+from pathlib import Path
+from src.core.paths import MEDIA_ROOT
+
+
+MEDIA_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp', '.tiff']
+
 
 class AvatarRandomizer:
     def __init__(self):
-        self.avatars = ["default_avatar1", "default_avatar2", "default_avatar3", "default_avatar4"]
+        self.current_dir = MEDIA_ROOT / "default_avatars"
+        
+        self.avatars = [
+            f for f in self.current_dir.iterdir() 
+            if f.is_file() and f.suffix.lower() in MEDIA_EXTENSIONS
+        ]
 
-    def get_random_avatar(self):
-        current_avatar = choice(self.avatars)
-        current_avatar = "media/" + "default_avatars/" + current_avatar + ".png"
-        return current_avatar
+    def get_random_avatar(self) -> str:
+        if not self.avatars:
+            return None
+            
+        random_file = choice(self.avatars)
+        
+        return f"media/default_avatars/{random_file.name}"
+the 
