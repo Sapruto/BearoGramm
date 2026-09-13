0fb38d671b8019e89b31cca92d680f0ab2c287b4
Sapruto <alex.sapryka@gmail.com>
2026-09-13 15:14:16 +0300

    I fixed run

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,19 @@
+from urllib.parse import urlparse
 from src.app import create_app
+from src.core.settings import Settings
 import uvicorn
 
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    parsed = urlparse(Settings.BASE_URL)
+    host = parsed.hostname or "127.0.0.1"
+    port = parsed.port or 8000
+
+    uvicorn.run(
+        app,
+        host=host,
+        port=port,
+        reload=Settings.APP.DEBUG,
+        log_level=Settings.APP.LOG_LEVEL.lower(),
+    )
